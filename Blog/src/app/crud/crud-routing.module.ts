7c9bf558b9098ef1d394082357f3d5c0a1697cc9
jameslee0106 <leejames0106@gmail.com>
2023-10-26 14:36:30 -0700

    router configuration

--- Blog/src/app/crud/crud-routing.module.ts
+++ Blog/src/app/crud/crud-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { BlogFormComponent } from './blog-form/blog-form.component';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'blog-list', pathMatch: 'full'},
+  {path: 'blog-list', component: BlogListComponent}, 
+  {path: 'create-blog', component: BlogFormComponent},
+  {path: 'update-blog/:blogId', component: BlogFormComponent},
+  {path: 'view-blog-details/:blogId', component: BlogDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
